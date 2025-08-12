a3c291a1951f197e5aeab59a53bb2d0b3945824d
Elvis Pfützenreuter <epxx@epxx.co>
2025-08-11 21:49:53 -0300

    HexPrint

--- goalarmeitbl/protocol.go
+++ goalarmeitbl/protocol.go
@@ -1,5 +1,9 @@
 package goalarmeitbl
 
+import (
+	"encoding/hex"
+)
+
 // Calcula checksum de frame longo
 // Presume que "dados" contém o byte de comprimento mas não contém o byte de checksum
 func Checksum(dados []byte) byte {
@@ -9,4 +13,17 @@ func Checksum(dados []byte) byte {
     }
     chksum = chksum ^ uint8(0xff)
     return chksum
+}
+
+func HexPrint(buf []byte) string {
+    s := hex.EncodeToString(buf)
+    ss := ""
+    for i := 0; i < len(s) - 2; i += 2 {
+        ss += s[i:i+2]
+        ss += " "
+    }
+    if len(s) > 0 {
+        ss += s[len(s)-2:]
+    }
+    return ss
 }
--- goalarmeitbl/protocol_test.go
+++ goalarmeitbl/protocol_test.go
@@ -10,3 +10,23 @@ func TestChecksum(t *testing.T) {
         t.Error("Checksum failed")
     }
 }
+
+func TestHexPrint(t *testing.T) {
+    pkt := []byte{0x74, 0x01, 0x35, 0x50, 0x26}
+    res := HexPrint(pkt)
+    if res != "74 01 35 50 26" {
+        t.Error("TextHexPrint failed I '" + res + "'")
+    }
+
+    pkt2 := []byte{0x74}
+    res2 := HexPrint(pkt2)
+    if res2 != "74" {
+        t.Error("TextHexPrint failed II '" + res2 + "'")
+    }
+
+    pkt3 := []byte(nil)
+    res3 := HexPrint(pkt3)
+    if res3 != "" {
+        t.Error("TextHexPrint failed III '" + res3 + "'")
+    }
+}
