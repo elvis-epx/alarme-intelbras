83ccb8719b4937df0da63cdecfe670ea64a80721
Elvis Pfützenreuter <epxx@epxx.co>
2025-08-12 21:50:28 -0300

    Completo 1º módulo Go

--- goalarmeitbl/protocol.go
+++ goalarmeitbl/protocol.go
@@ -2,6 +2,8 @@ package goalarmeitbl
 
 import (
 	"encoding/hex"
+	"fmt"
+    "slices"
 )
 
 // Calcula checksum de frame longo
@@ -36,16 +38,17 @@ func ContactIDDecode(dados []byte) (int, error) {
     posicao := 1
 
     for i := range dados {
-        digito := dados[len(dados) - 1 - i]
+        digito := int(dados[len(dados) - 1 - i])
 
         if digito == 0x0a { // zero
             // pass
         } else if digito >= 0x01 && digito <= 0x09 {
             numero += posicao * digito
         } else {
-            return -1, fmt.ErrorF("valor contact id invalido: %s", HexPrint(dados))
+            return -1, fmt.Errorf("valor contact id invalido: %s", HexPrint(dados))
         }
         posicao *= 10
+    }
 
     return numero, nil
 }
@@ -57,98 +60,121 @@ func ContactIDEncode(number int, length int) ([]byte) {
         number = -number
     }
 
-    for i in range length {
+    for i := range length {
         digit := number % 10
         number /= 10
         if digit == 0 {
             digit = 0x0a
         }
-        dados[length - 1 - i] = digit
+        dados[length - 1 - i] = byte(digit)
     }
 
     return dados
 }
 
+// Converte um número de até 2 dígitos para BCD
 func BCD(n int) (byte, error) {
     if n > 99 || n < 0 {
-        return 0, fmt.ErrorF("valor invalido para BCD: %02x", n)
+        return 0, fmt.Errorf("valor invalido para BCD: %02x", n)
     }
-    return ((n / 10) << 4) + (n % 10)
+    return byte(((n / 10) << 4) + (n % 10)), nil
 }
 
+// Converte um número BCD de tamanho arbitrário em inteiro
 func FromBCD(dados []byte) (int, error) {
     numero := 0
     posicao := 1
     
     for i := range dados {
-        nibbles := dados[len(dados) - 1 - i]
-        numero += (nibbles >> 4) * 10 * posicao
-        numero += (nibbles & 0x04) * posicao
+        nibbles := int(dados[len(dados) - 1 - i])
+        nibble1 := nibbles >> 4
+        nibble2 := nibbles & 0x0f
+        if nibble1 > 9 || nibble2 > 9 {
+            return 0, fmt.Errorf("Codigo BCD invalido: %02x", nibbles)
+        }
+        numero += nibble1 * 10 * posicao
+        numero += nibble2 * posicao
         posicao *= 100
     }
 
     return numero, nil
 }
 
-
-    # Codifica um número de 16 bits em 2 octetos
-    def be16(self, n):
-        return [ n // 256, n % 256 ]
+// Codifica um número de 16 bits em 2 octetos big-endian
+func BE16(n int) ([]byte) {
+    dados := []byte{byte(n / 256), byte(n % 256)}
+    return dados
+}
     
-    # Decodifica um buffer de 2 octetos para inteiro de 16 bits
-    def parse_be16(self, buf):
-        return buf[0] * 256 + buf[1]
+// Decodifica um inteiro big endian de exatamente 2 octetos
+func ParseBE16(dados []byte) (int) {
+    return int(dados[0]) * 256 + int(dados[1])
+}
 
-    def pacote_isecnet2(self, cmd, payload):
-        # ID da central, sempre zero
-        dst_id = self.be16(0x0000)
-        # ID nosso, pode ser qualquer número, devolvido nos pacotes de retorno
-        # Possivelmente uma relíquia de canais seriais onde múltiplos receptores
-        # ouvem as mensagens, e dst_id ajudaria a identificar o recipiente
-        src_id = self.be16(0x8fff)
-        length = self.be16(len(payload) + 2)
-        cmd_enc = self.be16(cmd)
-        pacote = dst_id + src_id + length + cmd_enc + payload
-        pacote = pacote + [ self.checksum(pacote) ]
-        return pacote
-
-    def pacote_isecnet2_auth(self, senha, tam_senha):
-        # 0x02 software de monitoramento, 0x03 mobile app
-        sw_type = [ 0x02 ]
-        senha = self.contact_id_encode(senha, tam_senha)
-        sw_ver = [ 0x10 ]  # nibble.nibble (0x10 = 1.0)
-        payload = sw_type + senha + sw_ver
-        return self.pacote_isecnet2(0xf0f0, payload)
-
-    # Retorna o comprimento de um pacote, se houver um pacote completo no buffer
-    # Se não, retorna 0
-    def pacote_isecnet2_completo(self, data):
-        # Um pacote tem tamanho mínimo 9 (src_id, dst_id, len, cmd, checksum)
-        if len(data) < 9:
-            return 0
-        compr = 6 + self.parse_be16(data[4:6]) + 1
-        if len(data) < compr:
-            return 0
-        return compr
-
-    # Consiste um pacote do protocolo ISECNet2
-    def pacote_isecnet2_correto(self, pct):
-        compr_liquido = self.parse_be16(pct[4:6])
-        if compr_liquido < 2:
-            # Um pacote deveria ter no minimo um comando
-            return False
-        # Algoritmo de checksum tem propriedade interessante:
-        # checksum de pacote sufixado com checksum resulta em 0
-        return self.checksum(pct) == 0x00
+func PacoteIsecNet2(cmd int, payload []byte) []byte {
+    // ID da central, sempre zero
+    dst_id := BE16(0x0000)
     
-    # Interpreta um pacote do protocolo ISECNet2
-    def pacote_isecnet2_parse(self, pct):
-        compr_liquido = self.parse_be16(pct[4:6])
-        compr_payload = compr_liquido - 2
-        cmd = self.parse_be16(pct[6:8])
-        payload = pct[8:8+compr_payload]
-        return cmd, payload
+    // ID nosso, pode ser qualquer número, devolvido nos pacotes de retorno
+    // Possivelmente uma relíquia de canais seriais onde múltiplos receptores
+    // ouvem as mensagens, e dst_id ajudaria a identificar o recipiente
+    src_id := BE16(0x8fff)
+    length := BE16(len(payload) + 2)
+    cmd_enc := BE16(cmd)
 
-    def pacote_isecnet2_bye(self):
-        return self.pacote_isecnet2(0xf0f1, [])
+    pacote := slices.Concat(dst_id, src_id, length, cmd_enc, payload)
+    return append(pacote, Checksum(pacote))
+}
 
+// Pacote de autenticação ISECNet2
+func PacoteIsecNet2Auth(senha int, tamanho_senha int) []byte {
+    // 0x02 software de monitoramento, 0x03 mobile app
+    sw_type := []byte{ 0x02 }
+    enc_senha := ContactIDEncode(senha, tamanho_senha)
+    sw_ver := []byte{ 0x10 } // nibble.nibble (0x10 = 1.0)
+
+    payload := slices.Concat(sw_type, enc_senha, sw_ver)
+    return PacoteIsecNet2(0xf0f0, payload)
+}
+
+// Retorna o comprimento de um pacote, se houver um pacote completo no buffer
+// Se não, retorna 0
+func PacoteIsecNet2Completo(dados []byte) int {
+    // Um pacote tem tamanho mínimo 9 (src_id, dst_id, len, cmd, checksum)
+    if len(dados) < 9 {
+        return 0
+    }
+    comprimento := 6 + ParseBE16(dados[4:6]) + 1
+    if len(dados) < comprimento {
+        return 0
+    }
+    return comprimento
+}
+
+// Consiste um pacote do protocolo ISECNet2 e informa se ele é válido
+// Exige que tenha sido pré-testado com PacoteIsecNet2Completo()
+func PacoteIsecNet2Correto(dados []byte) bool {
+    comprimento_liquido := ParseBE16(dados[4:6])
+    if comprimento_liquido < 2 {
+        // Um pacote deveria ter no minimo um comando
+        return false
+    }
+        
+    // Checksum de pacote já sufixado com checksum é igual a zero   
+    return Checksum(dados) == 0
+}
+ 
+// Interpreta um pacote do protocolo ISECNet2
+// Exige que tenha sido pré-testado com PacoteIsecNet2{Correto, Completo}()
+func PacoteIsecNet2Parse(dados []byte) (int, []byte) {
+    comprimento_liquido := ParseBE16(dados[4:6])
+    comprimento_payload := comprimento_liquido - 2
+    cmd := ParseBE16(dados[6:8])
+    payload := dados[8:8+comprimento_payload]
+
+    return cmd, payload
+}
+
+func PacoteIsecNet2Bye() []byte {
+    return PacoteIsecNet2(0xf0f1, nil)
+}
--- goalarmeitbl/protocol_test.go
+++ goalarmeitbl/protocol_test.go
@@ -2,12 +2,14 @@ package goalarmeitbl
 
 import (
     "testing"
+    "bytes"
 )
 
 func TestChecksum(t *testing.T) {
     pkt := []byte{0x74, 0x01, 0x35, 0x50, 0x26}
     if Checksum(pkt) != 201 {
-        t.Error("Checksum failed")
+        t.Error("failed")
+        return
     }
 }
 
@@ -15,18 +17,179 @@ func TestHexPrint(t *testing.T) {
     pkt := []byte{0x74, 0x01, 0x35, 0x50, 0x26}
     res := HexPrint(pkt)
     if res != "74 01 35 50 26" {
-        t.Error("TextHexPrint failed I '" + res + "'")
+        t.Error("failed I '" + res + "'")
+        return
     }
 
     pkt2 := []byte{0x74}
     res2 := HexPrint(pkt2)
     if res2 != "74" {
-        t.Error("TextHexPrint failed II '" + res2 + "'")
+        t.Error("failed II '" + res2 + "'")
+        return
     }
 
     pkt3 := []byte(nil)
     res3 := HexPrint(pkt3)
     if res3 != "" {
-        t.Error("TextHexPrint failed III '" + res3 + "'")
+        t.Error("failed III '" + res3 + "'")
+        return
+    }
+}
+
+func TestContactIDDecode(t *testing.T) {
+    res, err := ContactIDDecode([]byte{0x01, 0x0a, 0x03})
+    if res != 103 || err != nil {
+        t.Errorf("failed I %d", res)
+        return
+    }
+
+    res, err = ContactIDDecode([]byte{0x0a, 0x0a, 0x03})
+    if res != 3 || err != nil {
+        t.Errorf("failed II %d", res)
+        return
+    }
+
+    res, err = ContactIDDecode([]byte{0x0a, 0x0b, 0x03})
+    if err == nil {
+        t.Errorf("failed III")
+        return
+    }
+    res, err = ContactIDDecode([]byte{0x0a, 0x00, 0x03})
+    if err == nil {
+        t.Errorf("failed IV")
+        return
+    }
+}
+
+func TestContactIDEncode(t *testing.T) {
+    res := ContactIDEncode(103, 3)
+    if !bytes.Equal(res, []byte{0x01, 0x0a, 0x03}) {
+        t.Errorf("failed I ")
+        return
+    }
+}
+
+func TestBCD(t *testing.T) {
+    res, err := BCD(89)
+    if res != 0x89 || err != nil {
+        t.Errorf("failed I %x", res)
+        return
+    }
+
+    res, err = BCD(120)
+    if err == nil {
+        t.Errorf("failed II %x", res)
+        return
+    }
+}
+
+func TestFromBCD(t *testing.T) {
+    res, err := FromBCD([]byte{0x89})
+    if res != 89 || err != nil {
+        t.Errorf("failed I %d", res)
+        return
+    }
+
+    res, err = FromBCD([]byte{0x89, 0x01})
+    if res != 8901 || err != nil {
+        t.Errorf("failed II %d", res)
+        return
+    }
+
+    res, err = FromBCD([]byte{0xee})
+    if err == nil {
+        t.Errorf("failed III %d", res)
+        return
+    }
+}
+
+func TestPacoteIsecNet2Auth(t *testing.T) {
+    res := PacoteIsecNet2Auth(123456, 6)
+    if !bytes.Equal(res, []byte{0, 0, 143, 255, 0, 10, 240, 240, 2, 1, 2, 3, 4, 5, 6, 16, 144}) {
+        t.Errorf("failed I")
+        return
+    }
+
+    res = PacoteIsecNet2Auth(1034, 4)
+    if !bytes.Equal(res, []byte{0, 0, 143, 255, 0, 8, 240, 240, 2, 1, 10, 3, 4, 16, 153}) {
+        t.Errorf("failed II")
+        return
+    }
+}
+
+func TestPacoteIsecNet2Completo(t *testing.T) {
+    pacote := PacoteIsecNet2Auth(123456, 6)
+
+    res := PacoteIsecNet2Completo(nil)
+    if res > 0 {
+        t.Errorf("failed nil")
+    }
+
+    for i := range len(pacote) - 1 {
+        res = PacoteIsecNet2Completo(pacote[:i])
+        if res > 0 {
+            t.Errorf("failed %d", i)
+        }
+    }
+
+    res = PacoteIsecNet2Completo(pacote[:len(pacote)])
+    if res == 0 {
+        t.Errorf("failed full")
+    }
+}
+
+func TestPacoteIsecNet2Correto(t *testing.T) {
+    pacote := PacoteIsecNet2Auth(123456, 6)
+
+    for i := range len(pacote) - 1 {
+        if i < 6 {
+            // função exige pacotes completos em primeiro lugar
+            continue
+        }
+        res := PacoteIsecNet2Correto(pacote[:i])
+        if res {
+            t.Errorf("failed %d", i)
+        }
+    }
+
+    res := PacoteIsecNet2Correto(pacote[:len(pacote)])
+    if !res {
+        t.Errorf("failed full")
+    }
+}
+
+func TestPacoteIsecNet2Bye(t *testing.T) {
+    res := PacoteIsecNet2Bye()
+    if !bytes.Equal(res, []byte{0, 0, 143, 255, 0, 2, 240, 241, 140}) {
+        t.Errorf("failed I")
+        return
+    }
+}
+
+func TestPacoteIsecNet2(t *testing.T) {
+    res := PacoteIsecNet2(0xbabe, nil)
+    if !bytes.Equal(res, []byte{0, 0, 143, 255, 0, 2, 186, 190, 137}) {
+        t.Errorf("failed I")
+        return
+    }
+
+    res = PacoteIsecNet2(0xcafe, []byte{0xba, 0xbe})
+    if !bytes.Equal(res, []byte{0, 0, 143, 255, 0, 4, 202, 254, 186, 190, 187}) {
+        t.Errorf("failed II")
+        return
+    }
+}
+
+func TestPacoteIsecNet2Parse(t *testing.T) {
+    cmd, payload := PacoteIsecNet2Parse(PacoteIsecNet2(0xbabe, nil))
+    if cmd != 0xbabe || !bytes.Equal(payload, []byte{}) {
+        t.Errorf("failed I %x %d", cmd, len(payload))
+        return
+    }
+
+    cmd, payload = PacoteIsecNet2Parse(PacoteIsecNet2(0xcafe, []byte{0xba, 0xbe}))
+    if cmd != 0xcafe || !bytes.Equal(payload, []byte{0xba, 0xbe}) {
+        t.Errorf("failed II %x %d", cmd, len(payload))
+        return
     }
 }
