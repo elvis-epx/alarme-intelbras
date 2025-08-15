28ff403e8a14bbc9ecdf20d447b07979b5183fb9
Elvis Pfützenreuter <epxx@epxx.co>
2025-08-14 22:24:57 -0300

    Initial impl of TCP

---
+++ goalarmeitbl/tcpclient.go
@@ -0,0 +1,110 @@
+import (
+    "fmt"
+    "net"
+    "os"
+    "time"
+    "log"
+    "slices"
+)
+
+type TCPClientHandler struct {
+    Events chan Event
+    RecvBuf []byte
+
+    to_send chan []byte
+    conn *net.TCPConn
+}
+
+func NewTCPClientHandler(addr string) *TCPClientHandler {
+    h := make(TCPClientHandler{make(chan Event), nil, make(chan []byte, 1), nil})
+    go h.main(addr)
+    return h
+}
+
+// Main loop
+func (h *TCPClientHandler) main(addr string) {
+    go h.connect(addr)
+
+loop:
+    for {
+        evt := <-h.Events
+        log.Print("TCP handler: event ", evt.Name)
+        switch evt.Name {
+        case "connected":
+            defer h.conn.Close()
+            go h.recv()
+            go h.send()
+            h.Events <- {"Connected", nil}
+        case "connerr":
+            h.Events <- {"NotConnected", nil}
+        case "recv":
+            // from recv goroutine
+            h.recvbuf = slices.Concat(h.recvbuf, evt.Cargo)
+            h.Events <- {"Recv", nil}
+        case "send":
+            // forward to send goroutine
+            h.to_send <- evt.Cargo
+        case "err":
+            // notify higher layers
+            h.Events <- {"Err", nil}
+        case "eof":
+            // notify higher layers
+            h.Events <- {"Eof", nil}
+        case "Bye":
+            break loop
+        default:
+            // try to delegate handling 
+            if !h.event(evt) {
+                log.Fatal("Unhandled event ", evt.Name)
+            }
+        }
+    }
+}
+func (h *TCPClientHandler) connect(addr string) {
+    h.conn, err = net.Dial("tcp", addr)
+    if err != nil {
+        h.Events <- {"connerr", nil}
+        return
+    }
+    h.Events <- {"connected", nil}
+}
+
+// Data receiving goroutine
+func (h *TCPClientHandler) recv() {
+    for {
+        data := make([]byte, 1500)
+        n, err := h.conn.Read(data)
+        if err != nil {
+            if err == io.EOF {
+                h.Events <- Event{"eof", nil}
+            } else {
+                h.Events <- Event{"err", nil}
+            }
+            break
+        }
+        log.Print("Received ", n)
+        h.Events <- Event{"recv", data[:n]}
+    }
+}
+
+// Data sending goroutine
+func (h *TCPClientHandler) send() {
+    for {
+        data := <- h.to_send
+
+        for len(data) > 0 {
+            log.Print("Sending ", len(data))
+            n, err := h.conn.Write(data)
+            if err != nil {
+                if err == io.EOF {
+                    h.Events <- Event{"eof", nil}
+                } else {
+                    h.Events <- Event{"err", nil}
+                }
+                break
+            }
+            log.Print("Sent ", n)
+            data = data[n:]
+        }
+    }
+}
